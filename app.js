3bd7ef85849ec14713207845af2f0c5f825742e4
Don Avworo <theprodigaldon@icloud0.com>
2023-03-05 21:18:05 +0000

    added delete all button to the list

--- app.js
+++ app.js
@@ -1,61 +1,88 @@
-let closeIcon = document.getElementsByTagName("li");   // Get all the list items in the document and store them in a variable
-for (let i = 0; i <closeIcon.length; i++) {           // Loop through the list items and add a close button to each one of them 
-  let span = document.createElement("SPAN");            // Create a span element that will contain the close button in each list item //SEE NOTE-1 BELOW
-  let txt = document.createTextNode("\u00D7");          // Create a text node that will contain the close button in each list item //SEE NOTE-2 BELOW
-  span.className = "close";                             // Give the span element a class name of "close" so that it can be styled with CSS
-  span.appendChild(txt);                                // Append the text node to the span element. the text node contains the close button
-  closeIcon[i].appendChild(span);                      // Append the span element to the list item. The span element contains the close button 
+//add a close button to each list item by using appendChild() and createTextNode() methods //SEE notes.test NOTE - 1 
+let closeIcon = document.getElementsByTagName("li");    // Get all the list items in the document and store them in a variable
+for (let i = 0; i <closeIcon.length; i++) {             // Loop through the list items and add a close button to each one of them 
+    let span = document.createElement("SPAN");          // Create a span element that will contain the close button in each list item 
+    let txt = document.createTextNode("\u00D7");        // Create a text node that will contain the close button in each list item 
+    span.className = "close";                           // Give the span element a class name of "close" so that it can be styled with CSS
+    span.appendChild(txt);                              // Append the text node to the span element. the text node contains the close button
+    closeIcon[i].appendChild(span);                     // Append the span element to the list item. The span element contains the close button 
 }
-
-//NOTE-1: The span element is created and stored in a variable called span. The span element is then given a class name of "close" so that it can be styled with CSS.
-//The span element is then appended to the text node that contains the close button. The span element is then appended to the list item.
-
-//NOTE-2: The text node is created and stored in a variable called txt. The text node contains the close button. The text node is then appended to the span element.
 
 // Click on a close button to hide the current list item
 let close = document.getElementsByClassName("close");   // Get all the close buttons in the document and store them in a variable
-for (let i = 0; i < close.length; i++) {      // Loop through the close buttons and add a click event listener to each one of them
-    close[i].onclick = function() {           // When a close button is clicked, do the following
-    let div = this.parentElement;             // Get the parent element of the close button and store it in a variable called div
-    div.style.display = "none";               // Set the display style of the parent element to "none" so that it is hidden
+for (let i = 0; i < close.length; i++) {                // Loop through the close buttons and add a click event listener to each one of them
+    close[i].onclick = function() {                     // When a close button is clicked, do the following 
+    let div = this.parentElement;                       // Get the parent element of the close button and store it in a variable called div
+    div.style.display = "none";                         // Set the display style of the parent element to "none" so that it is hidden
     }
 }
 
-// Add a new Todo list item when the user clicks on the "Add" button
+// create a new list item when clicking on the "Add" button
 
-function newElement() {                       // Create a function called newElement
+let addBtn = document.getElementById("btn");            // Get the add button in the document and store it in a variable called addBtn
+addBtn.addEventListener("click", newElement);           // Add an event listener to the add button. When the add button is clicked, call the newElement function
 
-    let li = document.createElement("li");    // Create a list item and store it in a variable called li
-    let inputValue = document.getElementById("userInput").value;  // Get the value of the input field and store it in a variable called inputValue
-    let tn = document.createTextNode(inputValue);  // Create a text node and store it in a variable called tn (text node). The text node contains the value of the input field
-
-    li.appendChild(tn);                       // Append the text node to the list item
-
-    if (inputValue === '') {                  // If the input field is empty, do the following
-        alert("You must write something!");   // Display an alert message
-    } else {                                  // If the input field is not empty, do the following
-        document.getElementById("todo-list").appendChild(li);  // Append the list item to the unordered list
-
+function newElement() {                                 // Define the newElement function 
+    // alert("Hello World!");                           // Display an alert message
+    let li = document.createElement("li");              // Create a new list item and store it in a variable called li
+    let inputValue = document.getElementById("userInput").value; // Get the value of the input field and store it in a variable called inputValue
+    let t = document.createTextNode(inputValue);        // Create a text node that contains the value of the input field and store it in a variable called t
+    li.appendChild(t);                                  // Append the text node to the list item
+    if (inputValue === '') {                            // If the input field is empty, do the following
+        alert("You must write something!");             // Display an alert message
+    } else {                                            // If the input field is not empty, do the following
+        document.getElementById("todo-list").appendChild(li); // Append the list item to the unordered list
     }
+    document.getElementById("userInput").value = "";    // Set the value of the input field to an empty string
 
-    document.getElementById("userInput").value = "";  // Clear the input field
+    let span = document.createElement("SPAN");          // Create a span element that will contain the close button in each list item
+    let txt = document.createTextNode("\u00D7");        // Create a text node that will contain the close button in each list item
+    span.className = "close";                           // Give the span element a class name of "close" so that it can be styled with CSS
+    span.appendChild(txt);                              // Append the text node to the span element. the text node contains the close button
+    li.appendChild(span);                               // Append the span element to the list item. The span element contains the close button
 
-    let span = document.createElement("SPAN");  // Create a span element and store it in a variable called span
-
-    let txt = document.createTextNode("\u00D7");  // Create a text node and store it in a variable called txt. The text node contains the close button
-
-    span.className = "close";                 // Give the span element a class name of "close" so that it can be styled with CSS
-
-    span.appendChild(txt);                    // Append the text node to the span element
-
-    li.appendChild(span);                     // Append the span element to the list item
-
-    for (let i = 0; i < close.length; i++) {  // Loop through the close buttons and add a click event listener to each one of them
-        close[i].onclick = function() {       // When a close button is clicked, do the following
-        let div = this.parentElement;         // Get the parent element of the close button and store it in a variable called div
-
-        div.style.display = "none";           // Set the display style of the parent element to "none" so that it is hidden
+    for (let i = 0; i < close.length; i++) {            // Loop through the close buttons and add a click event listener to each one of them
+        close[i].onclick = function() {                 // When a close button is clicked, do the following
+        let div = this.parentElement;                   // Get the parent element of the close button and store it in a variable called div
+        div.style.display = "none";                     // Set the display style of the parent element to "none" so that it is hidden
         }
     }
-};
+
+    //stop the button from submitting the ul element
+    event.preventDefault();
+
+    //add a completed class to the list item when it is clicked
+    let list = document.querySelector('ul');            // Get the unordered list and store it in a variable called list
+    list.addEventListener('click', function(ev) {       // Add an event listener to the unordered list. When a list item is clicked, do the following
+        if (ev.target.tagName === 'LI') {               // If the target of the click event is a list item, do the following
+            ev.target.classList.toggle('completed');    // Toggle the completed class on the list item that was clicked on 
+        }
+    }, false);                                         // Set the useCapture parameter to false so that the event is handled in the bubbling phase //SEE notes.test NOTE - 2
+
+    //add a delete all button to the page
+    let deleteAllBtn = document.createElement("button"); // Create a button element and store it in a variable called deleteAllBtn
+    let deleteAllBtnText = document.createTextNode("Delete All"); // Create a text node that contains the text "Delete All" and store it in a variable called deleteAllBtnText
+    deleteAllBtn.appendChild(deleteAllBtnText);        // Append the text node to the button element
+    document.body.appendChild(deleteAllBtn);           // Append the button element to the body element
+
+    //add an event listener to the delete all button
+    deleteAllBtn.addEventListener("click", deleteAll); // Add an event listener to the delete all button. When the delete all button is clicked, call the deleteAll function
+
+    //define the deleteAll function
+    function deleteAll() {                              // Define the deleteAll function
+        let list = document.getElementById("todo-list"); // Get the unordered list and store it in a variable called list
+        list.innerHTML = "";                            // Set the innerHTML of the unordered list to an empty string
+    }
+
+    // add a class to the delete all button when the mouse hovers over it
+    deleteAllBtn.addEventListener("mouseover", function() { // Add an event listener to the delete all button. When the mouse hovers over the delete all button, do the following
+        deleteAllBtn.classList.add("hover");              // Add the hover class to the delete all button
+    });
+
+
+
+
+        
+
+}
 
